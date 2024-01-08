bec10d9cd48aca5d98978b43711fec92627feb30
nobilitariy <134000822+nobilitariy@users.noreply.github.com>
2024-01-08 07:47:58 +0000

    commit

--- secondsite/main.js
+++ secondsite/main.js
@@ -7,15 +7,26 @@
 // const num3 = 0.424;
 // const reundedNum3 = Math.reumd(num3 * 100) / 100;
 //confirm("da?") ? alert("ok") : alert("no");
-const pr = prompt("cto?")
-switch (pr) {
-    case 'mand':
-        alert(100);
-        break;
-    case 'ban':
-    case 'gr':
-        alert(70);
-        break;
-    default:
-        alert("no");
+// const pr = prompt("cto?").toLowerCase();
+// switch (pr) {
+//     case 'mand':
+//         alert(100);
+//         break;
+//     case 'ban':
+//     case 'gr':
+//         alert(70);
+//         break;
+//     default:
+//         alert("no");
+// }
+function getMaxEl(arr) {
+    let max = arr[0];
+    for (let index = 1; index < arr.length-1; index++) {
+        if (arr[index]<arr[index+1]){
+            max = arr[index+1];
+        }
+    }
+    return max;
+    
 }
+console.log(getMaxEl([1,2,3,4,5,6,7,8,4,34,2,1,1]));
